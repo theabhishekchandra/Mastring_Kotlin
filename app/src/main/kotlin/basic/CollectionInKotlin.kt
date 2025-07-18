cc5ecd15d64a828a6f7a3f9eb21535f3ae942e8a
Abhishek Chandra <ac927920@gmail.com>
2025-07-18 14:24:27 +0530

    Implement Collection in Kotlin.

---
+++ app/src/main/kotlin/basic/CollectionInKotlin.kt
@@ -0,0 +1,68 @@
+package org.abhishek.app.basic
+
+class CollectionInKotlin {
+
+    /** List -> A list is an ordered collection of items. In Kotlin, lists can be either mutable (MutableList) or read-only (List). For list creation, use the standard library functions listOf() for read-only lists and mutableListOf() for mutable lists. To prevent unwanted modifications, obtain read-only views of mutable lists by casting them to List.*/
+
+    // MutableList
+    val systemInputs : MutableList<Int> = mutableListOf<Int>(1,2,3,4,5)
+
+    // Immutable (Read Only)
+    val sudoInputs : List<Int> = systemInputs
+
+    fun addNewSystemInputs(input : Int)  {
+        systemInputs.add(input)
+    }
+    fun getSystemInputs() : List<Int> {
+        return systemInputs
+    }
+
+    /** Set -> A set is an unordered collection that does not support duplicates. For creating sets, there are functions setOf() and mutableSetOf(). A read-only view of a mutable set can be obtained by casting it to Set.*/
+
+    // MutableSet
+    val setOfData : MutableSet<Int> = mutableSetOf()
+
+    // Immutable Set ( Read Only)
+    val setOfInputData : Set<Int> = setOfData
+
+    /** Map -> A map is a collection of key/value pairs, where each key is unique and is used to retrieve the corresponding value. For creating maps, there are functions mapOf() and mutableMapOf(). Using the to infix function makes initialization less noisy. A read-only view of a mutable map can be obtained by casting it to Map.*/
+
+    val mapOfData : MutableMap<Int, String> = mutableMapOf(1 to "one", 2 to "two", 3 to "three")
+
+    /** Filter Function -> filter function enables you to filter collections. It takes a filter predicate as a lambda-parameter. The predicate is applied to each element. Elements that make the predicate true are returned in the result collection.*/
+
+    val number = listOf<Int>(-6,-5,-4,-3,-2,1,0,1,2,3,4,5,6,7,8,9)
+
+    val positive = number.filter { item -> item > 0 }
+    val negative = number.filter { it < 0}
+
+    // Any Function -> If item contain at least one. Function any returns true if the collection contains at least one element that matches the given predicate.
+
+    val atLeastOnePositive = number.any { it > 0 }
+    val atLeastOneNegative = number.any { 0 < it }
+
+    // All Function -> Function all returns true if all elements in collection match the given predicate.
+    val allEven = number.all { it % 2 == 0 }            // 2
+
+    val allLess6 = number.all { it < 6 }
+
+    // Function none -> Function none returns true if there are no elements that match the given predicate in the collection.
+    val allEven1 = number.none { it % 2 == 1 }           // 2
+
+    val allLess7 = number.none { it > 6 }               // 3
+
+    // TODO : More read  associateBy, groupBy.
+
+    // associateBy — Creates a Map with unique keys
+    // Groups elements into lists by key ( duplicates are Groups all matching items into a list)
+
+    // partition -> The partition function splits the original collection into a pair of lists using a given predicate:
+    //Elements for which the predicate is true.
+    //Elements for which the predicate is false.
+    val evenOdd = number.partition { it % 2 == 0 }           // 2
+//    val (pos, neg) = number.partition { it > 0 } // 3
+
+}
+fun main() {
+
+}
--- app/src/main/kotlin/intro/BasicSyntax.kt
+++ app/src/main/kotlin/intro/BasicSyntax.kt
@@ -1,13 +1,32 @@
 package org.abhishek.app.intro
 
+import org.abhishek.app.basic.sum
+import kotlin.math.sqrt
+
 fun main() {
+
+    /** Lambda Functions -> Lambda functions ("lambdas") are a simple way to create functions ad-hoc. Lambdas can be denoted very concisely in many cases thanks to type inference and the implicit it variable. */
+
+    val upperCase1: (String) -> String = {
+        str: String -> str.uppercase()
+    }
+    val upperCase2: (String) -> String = { str -> str.uppercase() }
+    val upperCase3 = { str: String -> str.uppercase() }
+    val upperCase5: (String) -> String = { it.uppercase() }
+    val upperCase6: (String) -> String = String::uppercase
+
+    val order = Order(listOf(Item("Bread", 25.0F), Item("Wine", 29.0F), Item("Water", 12.0F)))
+
     val withValue = 1
     val myFirstName  = "Abhishek"
     val myLastName = "Chandra"
 
     println(withValue.with(withValue))
     println(myFirstName.get(2))
-    println(withValue.with(withValue))
+//    println(calculator(1,2, ::sum)) // :: is the notation that references a function by name in Kotlin.
+//    println(calculator(1,2){a,b -> a * b})
+    println(operation())
+    println(order.maxPricedItemValue())
 
 }
 
@@ -20,3 +39,20 @@ infix fun String.with(other: String) = this + " " + other
 
 // Operator Functions -> Certain functions can be "upgraded" to operators, allowing their calls with the corresponding operator symbol.
 operator fun String.get(range: IntRange) = substring(range)
+
+/** Higher-Order Functions -> A higher-order function is a function that takes another function as parameter and/or returns a function.*/
+
+fun calculator(a : Int, b : Int, operation: (Int, Int) -> Int) : Int = operation(a, b)
+
+fun operation() : (Int) -> Int {
+    return ::square
+}
+fun square(x: Int) = x * x
+
+data class Item(val title: String, val price: Float)
+
+data class Order(val items: Collection<Item>)
+
+fun Order.maxPricedItemValue(): Float = this.items.maxByOrNull { it.price }?.price ?: 0F
+
+
